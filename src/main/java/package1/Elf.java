623c4d13d234a08970f22366fcbb6769cc3ca874
adamlauterowski <adamlauterowski@gmail.com>
2023-06-22 11:34:45 +0200

    added documentation for the class Elf

--- src/main/java/package1/Elf.java
+++ src/main/java/package1/Elf.java
@@ -1,10 +1,18 @@
 package package1;
 
+/**
+ * Describes Hero "Elf"
+ */
 public class Elf extends Hero {
     private static String[] imiona = {"Erro", "Legolas", "Alfsol", "Kaldia", "Albrun", "Aubrie"};
     public Elf(String name, int health, int strength) {
         super(name, health, strength);
     }
+
+    /**
+     * Constructs an Elf
+     * @return
+     */
     public static Elf getInstance(){
         String imie = imiona[rand.nextInt(imiona.length)];
         int zdrowie = rand.nextInt(50,100);
@@ -12,6 +20,12 @@ public class Elf extends Hero {
         return new Elf(imie, zdrowie, sila);
     }
 
+    /**
+     * Randomizes the name, health and the strength of an Elf
+     * @param another
+     */
+
+    
     @Override
     public void attack(Hero another) {
         int damage = (int)(this.strength * rand.nextDouble(0.5,1));
