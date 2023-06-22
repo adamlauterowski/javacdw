91fcd87674dfff150666490358dc5ef3e6b03527
adamlauterowski <adamlauterowski@gmail.com>
2023-06-22 11:29:31 +0200

    added documentation for the class Hero

--- src/main/java/package1/Hero.java
+++ src/main/java/package1/Hero.java
@@ -2,11 +2,22 @@ package package1;
 
 import java.util.Random;
 
+/**
+   Describes  Hero
+ */
+
 public abstract class Hero {
     public  String name;
     public int health;
     public int strength;
     protected static Random rand = new Random();
+
+    /**
+     * Constructs Hero
+     * @param name the hero name
+     * @param health hero's health points
+     * @param strength hero's attack strength
+     */
 
     public Hero(String name, int health, int strength) {
         this.name = name;
@@ -37,10 +48,13 @@ public abstract class Hero {
     public void setStrength(int strength) {
         this.strength = strength;
     }
-
-
-
+    
     public abstract void attack(Hero another);
+
+    /**
+     * attack method accepts hero as an argument
+     * @return
+     */
 
     @Override
     public String toString() {
