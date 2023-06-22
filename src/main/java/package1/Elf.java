82f99747208d798428b46139e644b9b12720886c
adamlauterowski <adamlauterowski@gmail.com>
2023-06-22 02:46:24 +0200

    adding all files

---
+++ src/main/java/package1/Elf.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class Elf extends Hero {
+    private static String[] imiona = {"Erro", "Legolas", "Alfsol", "Kaldia"};
+    public Elf(String name, int health, int strength) {
+        super(name, health, strength);
+    }
+    public static Elf getInstance(){
+        String imie = imiona[rand.nextInt(imiona.length)];
+        int zdrowie = rand.nextInt(50,100);
+        int sila = rand.nextInt(20,40);
+        return new Elf(imie, zdrowie, sila);
+    }
+
+    @Override
+    public void attack(Hero another) {
+        int damage = (int)(this.strength * rand.nextDouble(0.5,1));
+        another.health -= damage;
+        System.out.println(this.name + " attacks " + another.name +
+                " and deals " +damage+ " points of damage.");
+    }
+}
---
+++ src/main/java/package1/Hero.java
@@ -0,0 +1,51 @@
+package package1;
+
+import java.util.Random;
+
+public abstract class Hero {
+    public   String name;
+    public int health;
+    public int strength;
+    protected static Random rand = new Random();
+
+    public Hero(String name, int health, int strength) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public abstract void attack(Hero another);
+
+    @Override
+    public String toString() {
+        return "Hero{" +
+                "name='" + name + '\'' +
+                ", health=" + health +
+                ", strength=" + strength +
+                '}';
+    }
+}
